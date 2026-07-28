1ca6b09269672fa73a12210e9c57fcde3782faec
Viacheslav-Bo <zotikivna@gmail.com>
2026-07-28 20:17:17 +0300

    test render

--- src/middlewares/httpLogger.ts
+++ src/middlewares/httpLogger.ts
@@ -1,3 +1,31 @@
+// import { pinoHttp } from 'pino-http';
+
+// export const httpLogger = pinoHttp({
+//   level: 'info',
+//   redact: {
+//     paths: ['req.headers.authorization', 'req.headers.cookie'],
+//     censor: '[Redacted]',
+//   },
+//   serializers: {
+//     req(req) {
+//       return { method: req.method, url: req.url };
+//     },
+//     res(res) {
+//       return { statusCode: res.statusCode };
+//     },
+//   },
+//   transport: {
+//     target: 'pino-pretty',
+//     options: {
+//       colorize: true,
+//       translateTime: 'HH:MM:ss',
+//       ignore: 'pid,hostname,req,res,responseTime',
+//       messageFormat:
+//         '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+//       hideObject: true,
+//     },
+//   },
+// });
 import { pinoHttp } from 'pino-http';
 
 export const httpLogger = pinoHttp({
@@ -14,15 +42,17 @@ export const httpLogger = pinoHttp({
       return { statusCode: res.statusCode };
     },
   },
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname,req,res,responseTime',
-      messageFormat:
-        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
-    },
-  },
+  transport:
+    process.env.NODE_ENV !== 'production'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid,hostname,req,res,responseTime',
+            messageFormat:
+              '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+          },
+        }
+      : undefined,
 });
