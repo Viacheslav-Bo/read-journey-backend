ba487181455645930439c4476abe19f8cbe310da
Viacheslav-Bo <zotikivna@gmail.com>
2026-07-28 13:46:56 +0300

    chore: remove old single-file service stubs, replaced by dedicated folders

--- src/services/books.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '../prisma/client.js';
-
-export const getAllBooks = async () => {
-  return await prisma.libraryBook.findMany();
-};
-
-export const getBookById = async (id: string) => {
-  return prisma.libraryBook.findUnique({ where: { id } });
-};
