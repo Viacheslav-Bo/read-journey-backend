e986ecb6734108fb5b69ed6996d88cfd8d72ee95
Viacheslav-Bo <zotikivna@gmail.com>
2026-07-29 10:49:07 +0300

    docs: add Postman collection for manual API testing

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 
 export const authenticate = (
   req: Request,
-  res: Response,
+  _res: Response,
   next: NextFunction,
 ) => {
   const authHeader = req.headers.authorization;
