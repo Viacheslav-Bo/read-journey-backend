8a60adcad166624dcc8c3395711dfc5dbffc48c6
Viacheslav-Bo <zotikivna@gmail.com>
2026-07-31 22:09:40 +0300

    chore: update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,18 @@ import readingRouter from './routes/reading.js';
 
 const app = express();
 
+const allowedOrigins = [process.env.FRONTEND_DOMAIN, 'http://localhost:5173'];
+
 app.use(httpLogger);
 app.use(
   cors({
-    origin: process.env.FRONTEND_DOMAIN,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   }),
 );
