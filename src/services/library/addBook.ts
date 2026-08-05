50a8e13cc4e89cab2eed1ac9b19c6674d84d9681
Viacheslav-Bo <zotikivna@gmail.com>
2026-08-05 12:22:44 +0300

    fix addBook service

--- src/services/library/addBook.ts
+++ src/services/library/addBook.ts
@@ -1,14 +1,29 @@
 import prisma from '../../prisma/client.js';
+import { Prisma } from '@prisma/client';
+import createHttpError from 'http-errors';
 import type { z } from 'zod';
 import type { addBookSchema } from '../../validations/libraryValidation.js';
 
 type AddBookBody = z.infer<typeof addBookSchema>;
 
-export const addBookToLibrary = (userId: string, data: AddBookBody) => {
-  return prisma.libraryBook.create({
-    data: {
-      ...data,
-      userId,
-    },
-  });
+export const addBookToLibrary = async (userId: string, data: AddBookBody) => {
+  try {
+    return await prisma.libraryBook.create({
+      data: {
+        ...data,
+        userId,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === 'P2002'
+    ) {
+      throw createHttpError(409, 'This book is already in your library');
+    }
+
+    throw err;
+  }
 };
